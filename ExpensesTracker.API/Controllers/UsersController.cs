bb7929fd023387ab561160090ab4d83d8592ddcf
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-04 20:50:23 +0530

    add scalar and configured ef core in the project

---
+++ ExpensesTracker.API/Controllers/UsersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExpensesTracker.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAllUsers()
+        {
+            List<string> users = new List<string>() { "A", "B", "C" };
+            return Ok(users);
+        }
+
+    }
+}
