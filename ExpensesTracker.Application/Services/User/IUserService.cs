cdee998b2abaab833ea49c5fc40d98d196846f04
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-06 19:20:17 +0530

    added dtos and services for users in application layer

---
+++ ExpensesTracker.Application/Services/User/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using ExpensesTracker.Application.DTO.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Application.Services.User
+{
+    public interface IUserService
+    {
+        Task<UserResponse> CreateUser(CreateUserRequest request);
+    }
+}
