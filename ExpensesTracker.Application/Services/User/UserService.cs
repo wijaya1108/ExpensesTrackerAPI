d6066207a783d62cacd11932aacda2a06805c986
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-07 18:41:45 +0530

    add create user endpoint with its application logic, repo method

--- ExpensesTracker.Application/Services/User/UserService.cs
+++ ExpensesTracker.Application/Services/User/UserService.cs
@@ -1,4 +1,6 @@
 ﻿using ExpensesTracker.Application.DTO.User;
+using ExpensesTracker.Application.Mappers;
+using ExpensesTracker.Domain.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,21 @@ namespace ExpensesTracker.Application.Services.User
 {
     public class UserService : IUserService
     {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public async Task<UserResponse> CreateUser(CreateUserRequest request)
         {
-            throw new NotImplementedException();
+            var user = request.MapToUser(); //extension method to map to user model
+            var newUser = await _userRepository.InsertUser(user);
+            UserResponse userResponse = newUser.MapToUserResponse();
+            return userResponse;
+
+            //todo - hash password, validation
         }
     }
 }
