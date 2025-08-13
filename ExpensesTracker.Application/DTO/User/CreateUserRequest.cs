b127e62a1fd06ce7e8b93ec935565ca1c2365169
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-13 18:58:49 +0530

    added data annotations to createUser record for validation

--- ExpensesTracker.Application/DTO/User/CreateUserRequest.cs
+++ ExpensesTracker.Application/DTO/User/CreateUserRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,18 @@ namespace ExpensesTracker.Application.DTO.User
 {
     public record CreateUserRequest(
         string? FirstName,
+
+        [Required(ErrorMessage = "Last name is required")]
         string LastName,
+
         string? Address,
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         string Email,
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(4, ErrorMessage = "Password must be at least contain 4 characters")]
         string Password
         );
 }
