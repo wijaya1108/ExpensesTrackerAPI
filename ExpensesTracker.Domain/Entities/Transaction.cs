06d453e54e6de88b5d428350558408282a60029c
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-29 22:04:03 +0530

    added transction entity in domain layer

---
+++ ExpensesTracker.Domain/Entities/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Domain.Entities
+{
+    public class Transaction
+    {
+        public Guid UID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public Guid ModifiedBy { get; set; }
+        public DateTime DeletedDate { get; set; }
+        public Guid DeletedBy { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public Guid UserUID { get; set; }
+        public User User { get; set; }
+    }
+}
