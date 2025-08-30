0e43ebcaf6127a9070060e94592c0a77b597f0b6
uditha bandara <wijaya.uditha98@gmail.com>
2025-08-30 08:21:47 +0530

    created the relationship between transaction and user entity

---
+++ ExpensesTracker.Infrastructure/Configurations/TransactionConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using ExpensesTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Infrastructure.Configurations
+{
+    public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder)
+        {
+            builder.ToTable("Transactions");
+
+            builder.HasKey(t => t.UID);
+
+            builder.Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(250);
+
+            builder.Property(t => t.Amount)
+                .IsRequired()
+                .HasColumnType("decimal(18,2)");
+
+            builder.Property(t => t.CreatedDate)
+                .IsRequired();
+
+            builder.Property(t => t.ModifiedDate)
+                .IsRequired();
+
+            builder.Property(t => t.DeletedDate);
+
+            builder.Property(t => t.IsDeleted)
+                .IsRequired();
+
+        }
+    }
+}
---
+++ ExpensesTracker.Infrastructure/Migrations/20250830023948_AddTransactionTableWithRelationships.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExpensesTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTransactionTableWithRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    UID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: true),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    UserUID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.UID);
+                    table.ForeignKey(
+                        name: "FK_Transactions_Users_UserUID",
+                        column: x => x.UserUID,
+                        principalTable: "Users",
+                        principalColumn: "UID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_UserUID",
+                table: "Transactions",
+                column: "UserUID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transactions");
+        }
+    }
+}
