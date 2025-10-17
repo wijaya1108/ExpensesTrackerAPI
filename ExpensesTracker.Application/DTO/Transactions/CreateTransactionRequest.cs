b149c075c791d6d8a9fc8ff7fdd09be362cc85e3
uditha bandara <wijaya.uditha98@gmail.com>
2025-10-17 17:43:59 +0530

    added validation for the create and update transaction records using data annotations

--- ExpensesTracker.Application/DTO/Transactions/CreateTransactionRequest.cs
+++ ExpensesTracker.Application/DTO/Transactions/CreateTransactionRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,18 @@ using System.Threading.Tasks;
 namespace ExpensesTracker.Application.DTO.Transactions
 {
     public record CreateTransactionRequest(
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
         string Name,
+
         string? Description,
+
+        [Required(ErrorMessage = "Amount is required")]
         decimal Amount,
+
         Guid UserUID,
+
+        [Required(ErrorMessage = "Transaction type is required")]
         int TransactionTypeId);
 }
--- ExpensesTracker.Application/DTO/Transactions/UpdateTransactionRequest.cs
+++ ExpensesTracker.Application/DTO/Transactions/UpdateTransactionRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,13 @@ using System.Threading.Tasks;
 namespace ExpensesTracker.Application.DTO.Transactions
 {
     public record UpdateTransactionRequest(
+
+        [Required(ErrorMessage = "Name is required")]
+        [MaxLength(100)]
         string Name,
+
         string? Description,
+
         decimal Amount
         );
 }
