f323a435e63e2ee8e36cc984f8887e23e3f44d86
uditha bandara <wijaya.uditha98@gmail.com>
2025-10-21 18:18:31 +0530

    added token service without implementation and jwt nuget package

---
+++ ExpensesTracker.Application/Services/JWT/ITokenService.cs
@@ -0,0 +1,14 @@
+﻿using ExpensesTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Application.Services.JWT
+{
+    public interface ITokenService
+    {
+        string CreateToken(Domain.Entities.User user);
+    }
+}
