fd4968b60798637a13b3b6d6189cb0781d3dd482
uditha bandara <wijaya.uditha98@gmail.com>
2025-11-07 18:46:31 +0530

    added refresh token services in application layer without any implementations

---
+++ ExpensesTracker.Application/Services/JWT/IRefreshTokenService.cs
@@ -0,0 +1,14 @@
+﻿using ExpensesTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Application.Services.JWT
+{
+    public interface IRefreshTokenService
+    {
+        
+    }
+}
---
+++ ExpensesTracker.Application/Services/JWT/RefreshTokenService.cs
@@ -0,0 +1,21 @@
+﻿using ExpensesTracker.Domain.Entities;
+using ExpensesTracker.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpensesTracker.Application.Services.JWT
+{
+    public class RefreshTokenService : IRefreshTokenService
+    {
+        //private readonly IRefreshTokenRepository _refreshTokenRepository;
+
+        //public RefreshTokenService(IRefreshTokenRepository refreshTokenRepository)
+        //{
+        //    _refreshTokenRepository = refreshTokenRepository;
+        //}
+
+    }
+}
